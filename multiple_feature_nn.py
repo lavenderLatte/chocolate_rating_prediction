0d02ed8afe3e77872c2d4c7e5bf2a83a641d124b
Hana Ra <hanara@Hanas-MacBook-Air.local>
2023-11-26 21:02:53 -0800

    factored out functions, added bean origin as feature

--- multiple_feature_nn.py
+++ multiple_feature_nn.py
@@ -3,6 +3,7 @@ import torch
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import preprocessing
+from text_to_onehotvect import onehotEncode, onehotEncode_fortest
 
 raw_data = pd.read_csv("./chocolate_bars.csv")
 cleaned_data = raw_data.dropna()  # cleaned
@@ -18,42 +19,26 @@ coco_pct_norm = (coco_pct_raw - coco_pct_mean) / \
     coco_pct_std  # mean normalization
 coco_pct = coco_pct_norm.reshape(-1, 1)  # input
 
+
 ingredients = cleaned_data['ingredients']
-ing_set = set()
-for ing_str in ingredients:
-    ing_list = ing_str.split(",")
-    for ing in ing_list:
-        ing_set.add(ing.strip())
-
-# print(ing_set)
-
-ing_dict = {}
-
-for idx, ing in enumerate(sorted(ing_set)):
-    # print(idx, ing)
-    ing_dict[ing] = idx
-
-# print(ing_dict)  # ingredient to number mapping
-
-
-num_rows = len(cleaned_data)
-num_ing = len(ing_set)
-ing_raw_data = np.zeros((num_rows, num_ing), dtype=float)
-
-for row_num, ing_str in enumerate(ingredients):
-    ing_list = ing_str.split(",")
-    for ing in ing_list:
-        idx = ing_dict[ing.strip()]
-        ing_raw_data[row_num][idx] += 1
-# print(ing_raw_data[53])
-
+ing_raw_data, num_ing, ing_dict = onehotEncode(ingredients)
 ing_raw = torch.tensor(ing_raw_data, dtype=torch.float)
 ing_mean = torch.mean(ing_raw)
 ing_std = torch.std(ing_raw)
 ing = (ing_raw - ing_mean)/ing_std
 # print("shape: ", x_mean.shape, x_std.shape)
 
-x = torch.cat([ing, coco_pct], dim=1)  # 7 ingredients + coco%
+borigin = cleaned_data['bean_origin']
+borigin_raw_data, num_borigin, borigin_dict = onehotEncode(borigin)
+borigin_raw = torch.tensor(borigin_raw_data, dtype=torch.float)
+borigin_mean = torch.mean(borigin_raw)
+borigin_std = torch.std(borigin_raw)
+borigin = (borigin_raw - borigin_mean)/borigin_std
+# print("num_borigin: ", num_borigin)
+# print("borigin: ", borigin)
+
+
+x = torch.cat([ing, coco_pct, borigin], dim=1)  # 7 ingredients + coco%
 
 raw_y = torch.tensor(cleaned_data['rating'].values, dtype=torch.float)
 y = raw_y.reshape(-1, 1)  # true output
@@ -69,7 +54,7 @@ class NeuralNetwork(torch.nn.Module):
         # torch.nn.Linear(4, 1) means 4 features 1 output
         self.hidden_size = 100
         self.linear0 = torch.nn.Linear(
-            num_ing+1, self.hidden_size, bias=True)  # 7 ingredients + coco%
+            num_ing+1+num_borigin, self.hidden_size, bias=True)  # 7 ingredients + coco% + bean origin
         self.linear1 = torch.nn.Linear(
             self.hidden_size, self.hidden_size, bias=True)
         self.linear2 = torch.nn.Linear(self.hidden_size, 1, bias=True)
@@ -91,7 +76,7 @@ criterion = torch.nn.MSELoss()  # Mean Squared Error
 optimizer = torch.optim.SGD(model.parameters(), lr=0.005)
 
 loss_array = []
-for epoch in range(5000):
+for epoch in range(2000):
     # forward pass
     pred_y = model(x)
     loss = criterion(pred_y, y)
@@ -125,32 +110,64 @@ for i in range(10):
 # plt.legend()
 # plt.show()
 
-coco_ptc_test_raw = [[77], [77], [77], [77], [77], [77], [77],  # single ingredients
-                     [77], [77], [77], [77],  # ingredients combos
-                     [10], [20], [30]]  # coco variants
-coco_ptc_test_raw = torch.tensor(coco_ptc_test_raw)
-coco_ptc_test = (coco_ptc_test_raw - coco_pct_mean)/coco_pct_std
+# coco_ptc_test_raw = [[77], [77], [77], [77], [77], [77], [77],  # single ingredients
+#                      [77], [77], [77], [77],  # ingredients combos
+#                      [10], [20], [30]]  # coco variants
+# coco_ptc_test_raw = torch.tensor(coco_ptc_test_raw)
+# coco_ptc_test = (coco_ptc_test_raw - coco_pct_mean)/coco_pct_std
 
 
-ing_test_raw = [[1, 0, 0, 0, 0, 0, 0],  # beans - highly valued ingredient
-                [0, 1, 0, 0, 0, 0, 0],  # cocoa butter - also important
-                [0, 0, 1, 0, 0, 0, 0],  # Lecithin
-                [0, 0, 0, 1, 0, 0, 0],  # sugar - second highest
-                [0, 0, 0, 0, 1, 0, 0],  # sweetner
-                [0, 0, 0, 0, 0, 1, 0],  # salt
-                [0, 0, 0, 0, 0, 0, 1],  # vanilla
-                [1, 0, 0, 1, 0, 0, 0],  # beans + sugar  - good combo
-                [1, 0, 0, 1, 1, 0, 0],  # beans + sugar + sweetner - good combo
-                [1, 1, 0, 1, 0, 0, 0],  # beans + sugar + cocoa butter - good combo
-                # beans + sugar + cocoa butter + vanilla - lowest combo
-                [1, 0, 0, 1, 0, 0, 0],
-                [1, 0, 0, 1, 0, 0, 0],  # beans + sugar + 30% coco
-                [1, 0, 0, 1, 0, 0, 0],  # beans + sugar + 60% coco
-                [1, 0, 0, 1, 0, 0, 0],  # beans + sugar + 90% coco
-                ]
-ing_test_raw = torch.tensor(ing_test_raw)
-ing_test = (ing_test_raw - ing_mean)/ing_std
+def create_test_vect(test_ing_list, test_cocoa_pct, test_borigin):
+    test_ing_onehot = torch.tensor(
+        onehotEncode_fortest(test_ing_list, ing_dict), dtype=torch.float)
+    test_borigin_onehot = torch.tensor(onehotEncode_fortest(
+        test_borigin, borigin_dict), dtype=torch.float)
 
-x_test = torch.cat([ing_test, coco_ptc_test], dim=1)
-y_test = model(torch.tensor(x_test, dtype=torch.float))  # y_pred
+    test_ing_onehot = (test_ing_onehot-ing_mean)/ing_std
+    test_cocoa_pct = torch.tensor(
+        [(test_cocoa_pct-coco_pct_mean)/coco_pct_std])
+    test_borigin_onehot = (test_borigin_onehot-borigin_mean)/borigin_std
+
+    print("shapes: ", test_ing_onehot.shape,
+          test_cocoa_pct.shape, test_borigin_onehot.shape)
+    test_vect = torch.cat(
+        [test_ing_onehot, test_cocoa_pct, test_borigin_onehot])
+    print("shapes: ", test_vect)
+
+    return test_vect
+
+
+test_vecs = []
+for key in ing_dict.keys():
+    test_vecs.append(create_test_vect([key], 77, ["Blend"]))
+
+test_vecs.append(create_test_vect(["B", "S"], 77, ["Blend"]))
+
+test_vecs = torch.stack(test_vecs)
+# test_vecs = torch.tensor(test_vecs)
+y_test = model(test_vecs)
 print(y_test)
+
+
+# ing_test_raw = [[1, 0, 0, 0, 0, 0, 0],  # beans - highly valued ingredient
+#                 [0, 1, 0, 0, 0, 0, 0],  # cocoa butter - also important
+#                 [0, 0, 1, 0, 0, 0, 0],  # Lecithin
+#                 [0, 0, 0, 1, 0, 0, 0],  # sugar - second highest
+#                 [0, 0, 0, 0, 1, 0, 0],  # sweetner
+#                 [0, 0, 0, 0, 0, 1, 0],  # salt
+#                 [0, 0, 0, 0, 0, 0, 1],  # vanilla
+#                 [1, 0, 0, 1, 0, 0, 0],  # beans + sugar  - good combo
+#                 [1, 0, 0, 1, 1, 0, 0],  # beans + sugar + sweetner - good combo
+#                 [1, 1, 0, 1, 0, 0, 0],  # beans + sugar + cocoa butter - good combo
+#                 # beans + sugar + cocoa butter + vanilla - lowest combo
+#                 [1, 0, 0, 1, 0, 0, 0],
+#                 [1, 0, 0, 1, 0, 0, 0],  # beans + sugar + 30% coco
+#                 [1, 0, 0, 1, 0, 0, 0],  # beans + sugar + 60% coco
+#                 [1, 0, 0, 1, 0, 0, 0],  # beans + sugar + 90% coco
+#                 ]
+# ing_test_raw = torch.tensor(ing_test_raw)
+# ing_test = (ing_test_raw - ing_mean)/ing_std
+
+# x_test = torch.cat([ing_test, coco_ptc_test], dim=1)
+# y_test = model(torch.tensor(x_test, dtype=torch.float))  # y_pred
+# print(y_test)
